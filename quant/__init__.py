8850e944aab353c49c87525fe30a93f92cd3bd91
zhetian592 <1852418657@qq.com>
2026-04-21 20:56:04 +0800

    Update __init__.py

--- quant/__init__.py
+++ quant/__init__.py
@@ -1,11 +1,12 @@
-# quant/__init__.py
-from .market_data import get_market_valuation, get_north_flow, get_market_risk_level
+"""
+量化数据模块入口
+"""
+
+from .market_data import get_market_risk_level
 from .fund_data import update_fund_nav
 from .risk_control import check_position_risk, get_risk_advice
 
 __all__ = [
-    "get_market_valuation",
-    "get_north_flow", 
     "get_market_risk_level",
     "update_fund_nav",
     "check_position_risk",
